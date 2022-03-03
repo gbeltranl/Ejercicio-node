42e2c477e542f7f884901288cfd9cd5864661633
gbeltranl <g.beltranl@uniandes.edu.co>
2022-03-03 14:15:43 -0500

    Finalizado

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
-const items = null;
-
-fs.readFile('products-ecommerce.json', 'utf-8', (err, data) => {
+var items = null;
+var jsonData = null;
+fs.readFile('Ejercicio-node/products-ecommerce.json', 'utf-8', (err, data) => {
   items = data;
+  const jsonData = JSON.parse(data);
+  fs.writeFile('Ejercicio-node/output.json', '', function (err) {
+    if (err) throw err;
+    console.log('Creado!');
+  });  jsonData.items.forEach(element => {
+    if(element.price.amount < 10000){
+      fs.appendFile('Ejercicio-node/output.json', JSON.stringify(element, null , 4) + '\n', function (err) {
+        if (err) throw err;
+      });
+    }
+  });
+ 
 })
