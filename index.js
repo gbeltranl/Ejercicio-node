9711578c5cbfc279481685d9a82f95599b38f63a
gbeltranl <g.beltranl@uniandes.edu.co>
2022-03-03 17:46:26 -0500

    Funcional con nodemon

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 var items = null;
 var jsonData = null;
-fs.readFile('Ejercicio-node/products-ecommerce.json', 'utf-8', (err, data) => {
-  items = data;
+fs.readFile('products-ecommerce.json', 'utf-8', (err, data) => {
   const jsonData = JSON.parse(data);
-  fs.writeFile('Ejercicio-node/output.json', '', function (err) {
-    if (err) throw err;
-    console.log('Creado!');
+  fs.writeFile('output.json', '', function (err) {
+    if (err) throw(err);
   });  jsonData.items.forEach(element => {
     if(element.price.amount < 10000){
-      fs.appendFile('Ejercicio-node/output.json', JSON.stringify(element, null , 4) + '\n', function (err) {
+      fs.appendFile('output.json', JSON.stringify(element, null , 4) + '\n', function (err) {
         if (err) throw err;
       });
     }
